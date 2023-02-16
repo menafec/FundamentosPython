ee2bc53cd34a6699135e40b47d703dae60d98ae6
Ximena Fernandez <124226344+menafec@users.noreply.github.com>
2023-02-16 12:21:42 -0600

    Add files via upload

---
+++ Fundamentos.py
@@ -0,0 +1,110 @@
+def nuevoTema(tema):
+    print("\n=====", tema, "=====\n")
+
+#para que el interprete sepa que es una funcion usamos def
+#para que el interprete sepa cuanto dura la funcion TABULAMOS
+
+nuevoTema("Enteros")
+w = 105
+x = 210829403
+y = -256
+z = 0b00110011
+h = 0xffa
+
+print(w, type (w))
+print(x, type (x))
+print(y, type (y))
+print(z, type (z))
+print(h, type (h))
+
+
+nuevoTema("Flotantes")
+x = 1297.50
+print(x, type(x))
+
+print("Hola")
+
+
+#para indicar que es complejo debe ser primero el numero y luego la j
+nuevoTema("Complejos")
+x = -46j
+y = 12 + 45j
+z = 2j
+print(x, type (x))
+print(y, type (y))
+print(z, type (z))
+
+
+nuevoTema("Variables")
+variable1 = 10
+_variable2 = 6.2456
+Variable3 = "Juancho"
+dosPalabras = "Hola"
+dos_palabras = "Hello" #estilo convencional de python
+print(variable1, Variable3, _variable2, dos_palabras, dosPalabras)
+
+
+nuevoTema("Booleans")
+lis = [8]
+print(lis, "es" , bool(lis))
+t = ()
+print(t,"es"  , bool(t))
+
+
+nuevoTema("Listas")
+a = [10, 20.5, "Python list"]
+print(a)
+print(a[1])
+a[0] = "hola"
+print (a)
+
+
+nuevoTema("Tuplas")
+t = (25, "Tupla", 1 + 10j, 3.1416)
+print(t)
+print(t[2])
+print("t[0:2]: ", t[0:2])
+#t[1] =34  OPERACION NO VALIDA EN TUPLAS, NO SE PUEDE SOBREESCRIBIR
+
+print("==== Operadores aritmeticos ====")
+print("Operador division entera (10//3): ",  10//3)
+print("Operador potencia (5 ** 3): ", 5**3)
+
+print("==== Operadores logicos ====")
+print ("Operador and (True and False): ", True and False) 
+
+#este es un comentario de una linea
+
+"""este es un 
+comentario 
+de varias
+lineas :)
+"""
+
+#Actividad: Imprimir la tabla de verdad de los operadores logicos
+print("==== Actividad 1 ====")
+print("==== and ====")
+print ("Operador and (True and False): ", True and False) 
+print ("Operador and (True and True): ", True and True) 
+print ("Operador and (False and False): ", False and False) 
+print ("Operador and (False and True): ", False and True) 
+
+print("==== or====")
+print ("Operador or (True or False): ", True or False) 
+print ("Operador or (True or True): ", True or True) 
+print ("Operador or (False or False): ", False or False) 
+print ("Operador or (False or True): ", False or True) 
+
+print("==== not ====")
+print ("Operador not (Not True): ", not True)
+print ("Operador not (Not False): ", not False)
+
+print("==== Operadores de comparacion ====")
+print(" 2 == 3" , 2==3)
+print(" 2 != 3" , 2!=3)
+print(" 2 < 3", 2<3 )
+print(" 2 > 3", 2>3 )
+print(" 2 <= 3", 2<=3 )
+print(" 2 >= 3", 2>=3 )
+
+
